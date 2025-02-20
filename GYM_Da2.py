5abc828f61738a395008f2f986d0e3746264ec77
Dani8448 <dalx8448@gmail.com>
2025-02-20 17:49:35 +0100

    Tabla clientes

--- GYM_Da2.py
+++ GYM_Da2.py
@@ -3,3 +3,13 @@ import sqlite3
 # Conectar a la base de datos (se crea si no existe)
 conn = sqlite3.connect("gym_da2.db")
 cursor = conn.cursor()
+
+#Tabla cientes
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS Clientes (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nombre TEXT NOT NULL,
+    edad INTEGER
+)
+""")
